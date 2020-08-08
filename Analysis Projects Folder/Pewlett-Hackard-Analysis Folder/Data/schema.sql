78afe0ee29afa6d5463c55a1d02e5c8568063f15
KL <kassielu@gmail.com>
2020-08-08 10:42:48 -0400

    Add changes up to 7.3.1

--- Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/schema.sql
+++ Analysis Projects Folder/Pewlett-Hackard-Analysis Folder/Data/schema.sql
@@ -52,7 +52,7 @@ emp_no INT NOT NULL,
     from_date DATE NOT NULL,
     to_date DATE NOT NULL,
 FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
-    PRIMARY KEY (emp_no)
+    PRIMARY KEY (emp_no, from_date, to_date)
 );
 
-select * from departments
+commit;
